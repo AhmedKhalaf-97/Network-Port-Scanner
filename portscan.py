1471b5b0c1ea7101cde7b0b950746d014d5828e5
AhmedKhalaf-97 <ahmedkhalafx97@gmail.com>
2025-11-05 08:25:23 -0800

    created a basic TCP port scanner

---
+++ portscan.py
@@ -0,0 +1,58 @@
+import sys
+import threading
+from socket import *
+
+
+def port_scan_TCP_task(hostname, port):
+    try:
+        scanner_socket = socket(AF_INET, SOCK_STREAM)
+        scanner_socket.settimeout(1)
+        connection_status = scanner_socket.connect_ex((hostname, port))
+        print(connection_status)
+    except Exception as e:
+        print("Failed to connect" + port)
+    finally:
+        scanner_socket.close()
+
+
+def port_scan_TCP(hostname, portlow, porthigh):
+    for port in range(portlow, porthigh + 1):
+        port_scan_TCP_task(hostname, port)
+
+
+def port_scan_UDP(hostname, portlow, porthigh):
+    print("Scanning UDP ports...")
+
+
+def main(argv):
+    # The program will accept command-line arguments:
+    if len(sys.argv) == 5:
+        try:
+            hostname = str(sys.argv[1])
+            protocol = str(sys.argv[2])
+            portlow = int(sys.argv[3])
+            porthigh = int(sys.argv[4])
+
+            if protocol.upper() == "TCP":
+                port_scan_TCP(hostname, portlow, porthigh)
+            elif protocol.upper() == "UDP":
+                port_scan_UDP(hostname, portlow, porthigh)
+            else:
+                # The program will terminate if incorrect protocol.
+                print(
+                    "Usage: python3 portscan.py <hostname> <protocol> <portlow> <porthigh>"
+                )
+                sys.exit(1)
+
+        except ValueError:
+            print(
+                "Invalid input. Please enter valid inputs.\nUsage: python3 portscan.py <hostname> <protocol> <portlow> <porthigh>"
+            )
+    else:
+        # The program will terminate if it there is anything else other than 4 arguments.
+        print("Usage: python3 portscan.py <hostname> <protocol> <portlow> <porthigh>")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
