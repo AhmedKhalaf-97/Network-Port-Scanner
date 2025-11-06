4158e8c64f0c16164706b08b2cf802b9e189d546
AhmedKhalaf-97 <ahmedkhalafx97@gmail.com>
2025-11-06 14:06:32 -0800

    Added comments

--- portscan.py
+++ portscan.py
@@ -1,55 +1,87 @@
+# Name: Ahmed Khalaf
+# Course: CSC138 Computer Network Fundamentals - SECTION 01
+# Date: 11/06/2025
+
+# Description:
+# This program implements a simple network port scanner that reports
+# the status of TCP and UDP ports for a given host within a user-defined
+# port range. It determines whether each port is open or closed using
+# socket connections and identifies the service name (when available).
+
+# The program accepts four command-line arguments:
+#   python3 portscan.py <hostname> <protocol> <portlow> <porthigh>
+
+
+# Import system and socket modules.
 import sys
 from socket import *
 
 
+# Function that scans a single TCP port. Accepts the hostname and the port.
 def port_scan_TCP(hostname, port):
+    # Try/Except block in case the socket times out or throws any exception.
     try:
+        # Open a TCP socket.
         scanner_socket = socket(AF_INET, SOCK_STREAM)
+        # Set timeout of 1 second.
         scanner_socket.settimeout(1)
+        # Attempt to connect to the port.
         connection_status = scanner_socket.connect_ex((hostname, port))
+        # If the connection is successful or if the port is open, the connection status will be 0, otherwise, it's closed.
         if connection_status == 0:
+            # Get the service name running on the port.
             try:
                 port_service_name = getservbyport(port, "tcp")
             except Exception as e:
-                # OSError
+                # OSError: in case the service name is not available.
                 port_service_name = "svc name unavail"
             print("port", port, "open   :", port_service_name)
         else:
             print("port", port, "closed")
     except Exception as e:
-        # timeout
+        # timeout: in case the connections times out.
         print("port", port, "closed")
     finally:
+        # After scanning, close the socket.
         scanner_socket.close()
 
 
+# Function that scans a single UDP port. Accepts the hostname and the port.
 def port_scan_UDP(hostname, port):
+    # Try/Except block in case the socket times out or throws any exception.
     try:
+        # Open a UDP socket.
         scanner_socket = socket(AF_INET, SOCK_DGRAM)
+        # Set timeout of 1 second.
         scanner_socket.settimeout(1)
-
+        # Attempt to send a query message to the port.
         scanner_socket.sendto("message".encode(), (hostname, port))
+        # If it recieves a response back, that means the UDP port is open.
         pongMsg, server_address = scanner_socket.recvfrom(2048)
-
+        # Check the response back.
         if pongMsg.decode() == "PONG":
+            # Get the service name running on the port.
             try:
                 port_service_name = getservbyport(port, "udp")
             except Exception as e:
-                # OSError
+                # OSError: in case the service name is not available.
                 port_service_name = "svc name unavail"
             print("port", port, "open   :", port_service_name)
         else:
             print("port", port, "closed")
     except Exception as e:
-        # timeout
+        # timeout: in case the connections times out.
         print("port", port, "closed")
     finally:
+        # After scanning, close the socket.
         scanner_socket.close()
 
 
+# Function that runs port scanner based on the specified protocol.
 def port_scanner(hostname, protocol, portlow, porthigh):
-    scanner_threads = list()
+    # Go through the ports, and scan them individually.
     for port in range(portlow, porthigh + 1):
+        # It will scan the ports based on the specified protocol.
         if protocol == "TCP":
             port_scan_TCP(hostname, port)
 
@@ -57,6 +89,7 @@ def port_scanner(hostname, protocol, portlow, porthigh):
             port_scan_UDP(hostname, port)
 
 
+# Function that checks the validity of the hostname.
 def check_hostname(hostname):
     try:
         gethostbyname(hostname)
@@ -65,8 +98,9 @@ def check_hostname(hostname):
         return False
 
 
+# The main entry point of the program.
 def main(argv):
-    # The program will accept command-line arguments:
+    # The program will accept 4 command-line arguments
     if len(sys.argv) == 5:
         try:
             hostname = str(sys.argv[1])
@@ -74,12 +108,14 @@ def main(argv):
             portlow = int(sys.argv[3])
             porthigh = int(sys.argv[4])
 
+            # Checks if the inputted hostname is valid.
             if check_hostname(hostname) == False:
                 print(
                     f"scanning host={hostname}, protocol={protocol}, ports: {portlow} -> {porthigh} error: host {hostname} does not exist"
                 )
                 sys.exit(1)
 
+            # Call the port scanner with the inputted values from the user.
             if protocol.upper() == "TCP":
                 port_scanner(hostname, "TCP", portlow, porthigh)
             elif protocol.upper() == "UDP":
@@ -92,6 +128,7 @@ def main(argv):
                 sys.exit(1)
 
         except ValueError:
+            # The program will terminate if inputted ports are not integer type.
             print(
                 "Invalid port input.\nUsage: python3 portscan.py <hostname> <protocol> <portlow> <porthigh>"
             )
